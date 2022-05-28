43a38da2037e78cd85b7c2c85d4540d4305deb2c
Dan Thackray <djthackray@gmail.com>
2022-05-28 17:25:46 +0100

    Added evaluation functionality

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let shouldResetScreen = false
 const numberButtons = document.querySelectorAll('.btn-number')
 const operatorButtons = document.querySelectorAll('.btn-operate')
 const screen = document.getElementById('screen-primary')
+const secondaryScreen = document.getElementById('screen-secondary')
 const clearButton = document.getElementById('btn-clear')
 const undoButton = document.getElementById('btn-undo')
 
@@ -27,6 +28,7 @@ function undoInput() {
 }
 
 function resetScreen() {
+    secondaryScreen.textContent = ''
     screen.textContent = 0
 }
 
@@ -43,10 +45,23 @@ function updateScreen(number) {
 }
 
 function setOperator(operator) {
+    if (currentOperation !== null) evaluate()
     argument1 = screen.textContent
     currentOperation = operator
-    lastOperationScreen.textContent = `${firstOperand} ${currentOperation}`
+    secondaryScreen.textContent = `${argument1} ${currentOperation}`
     shouldResetScreen = true
+  }
+
+  function evaluate() {
+    if (currentOperation === null || shouldResetScreen) return
+    if (currentOperation === '/' && screen.textContent === '0') {
+      alert("You can't divide by 0!")
+      return
+    }
+    argument2 = screen.textContent
+    screen.textContent = operate(argument1, argument2, currentOperation)
+    secondaryScreen.textContent = `${argument1} ${currentOperation} ${argument2} =`
+    currentOperation = null
   }
 
 function add (a, b) {
